d2e25659160dc2680472dc82b4e5ddfacb2b128f
Suhail Ulla Khan(suhailulla.khan) <suhailulla.khan@infosys.com>
2022-02-08 16:32:24 +0530

    Card filters added in search result page

--- src/page/Hintsearch.js
+++ src/page/Hintsearch.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { Layout } from "antd";
+import { Row, Col, Slider, Layout } from "antd";
 import MainFooter from "../components/MainFooter";
 import MainHeader from "../components/MainHeader";
 import { Route } from "react-router-dom";
-import { useRouteMatch,Redirect } from "react-router-dom";
+import { useRouteMatch, Redirect } from "react-router-dom";
 import RecentArticals from "../components/RecentArticals";
 import PopularTopics from "../components/PopularTopics";
 import MainSearch from "./MainSearch";
+import CollapsibleFilter from "../components/CollapsibleFilter";
 
 const { Content } = Layout;
 
 function HintSearch(props) {
   const match = useRouteMatch();
-  
+
   return (
     <React.Fragment>
       <MainHeader />
@@ -21,7 +22,13 @@ function HintSearch(props) {
           <MainSearch />
         </Route>
         <Route path={`${match.url}/search`} exact>
-            <h1>Search</h1>
+          <Row gutter={[16, 16]}>
+            <Col span={6}>
+              <CollapsibleFilter />
+            </Col>
+            <Col span={12} />
+            <Col span={6} />
+          </Row>
         </Route>
         <Redirect from={`${match.url}`} exact to={`${match.url}/mainsearch`} />
       </Content>
