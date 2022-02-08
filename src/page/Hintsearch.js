85d27c93cb5ca995a362f959d6358b673d58f1aa
Gajanan Mehta(gajanan.mehta) <gajanan.mehta@infosys.com>
2022-02-08 15:50:54 +0530

    search-result-page-card-change
    -worked on search result middle part

--- src/page/Hintsearch.js
+++ src/page/Hintsearch.js
@@ -2,28 +2,33 @@ import React from "react";
 import { Layout } from "antd";
 import MainFooter from "../components/MainFooter";
 import MainHeader from "../components/MainHeader";
-import { Route } from "react-router-dom";
-import { useRouteMatch,Redirect } from "react-router-dom";
-import RecentArticals from "../components/RecentArticals";
-import PopularTopics from "../components/PopularTopics";
+import { Route, Switch } from "react-router-dom";
+import { useRouteMatch, Redirect } from "react-router-dom";
 import MainSearch from "./MainSearch";
+import SearchResult from "./SearchResult";
 
 const { Content } = Layout;
 
 function HintSearch(props) {
   const match = useRouteMatch();
-  
+
   return (
     <React.Fragment>
       <MainHeader />
       <Content className="site-layout-background">
-        <Route path={`${match.url}/mainsearch`} exact>
-          <MainSearch />
-        </Route>
-        <Route path={`${match.url}/search`} exact>
-            <h1>Search</h1>
-        </Route>
-        <Redirect from={`${match.url}`} exact to={`${match.url}/mainsearch`} />
+        <Switch>
+          <Route path={`${match.url}/mainsearch`} exact>
+            <MainSearch />
+          </Route>
+          <Route path={`${match.url}/search`} exact>
+            <SearchResult />
+          </Route>
+          <Redirect
+            from={`${match.url}`}
+            to={`${match.url}/mainsearch`}
+            exact
+          />
+        </Switch>
       </Content>
       <MainFooter />
     </React.Fragment>
